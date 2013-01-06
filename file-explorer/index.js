15f56da37189638b59d4b350a5479b30dcd75956
jmarizgit <mm@emoriz.com>
2013-01-06 04:18:54 -0800

    refactoring

--- file-explorer/index.js
+++ file-explorer/index.js
@@ -1,5 +1,7 @@
 //module dependencies
-var fs  = require('fs') //file system module
+var fs  =   require('fs'), //file system module
+            stdin = process.stdin,
+            stdout = process.stdout 
 
 //list of present directories
 //console.log(fs.readdirSync(__dirname));
@@ -19,22 +21,26 @@ fs.readdir(process.cwd(),function(err,files){
 
     function file(i){
         var filename = files[i];
+
         fs.stat(__dirname+'/'+filename,function(err,stat){
             if(stat.isDirectory()){
                 console.log("\t"+i+"\033[36m - "+filename+"/\33[39m");
             }else{
                 console.log("\t"+i+"\033[90m - "+filename+"\33[39m");
             }//if_else
-            i++;
-            if(i == files.length){
-                console.log('');
-                process.stdout.write('\033[33mEnter your choice: \033[39m');
-                process.stdin.resume();
-                process.stdin.setEncoding('utf8');
+
+            if(++i == files.length){
+                read();
             }else{
                 file(i);
             }
         });//fs.stat()
     }//file()
+    function read(){
+        console.log('');
+        stdout.write('\033[33mEnter your choice: \033[39m');
+        stdin.resume();
+        stdin.setEncoding('utf8');
+    }
     file(0);
 });
