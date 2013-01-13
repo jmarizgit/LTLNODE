f6c98bd6f7938032c5aa925c066be18195fc5c62
jmarizgit <mm@emoriz.com>
2013-01-12 16:47:44 -0800

    wrapping up

--- node-chat/index.js
+++ node-chat/index.js
@@ -8,23 +8,32 @@ var server = net.createServer(function(conn) { // Create server
     console.log('\033[90m\tNew connection!\033[39m'); // handle connection
     conn.write('\n > Welcome to \033[92mnode-chat\033[39m!' + '\n' + count + ' other people are connected at this time.' + '\n > please write your name and press enter: ');
     count++; // increament connections counter
+    function broadcast(msg, exceptMyself){
+        for(var i in users){
+            if(!exceptMyself || i != nickname){
+                users[i].write(msg);
+            }
+        }
+    }//broadcast()
     conn.on('data', function(data){
         data = data.replace('\r\n',''); // remove enter chars
         if(!nickname) {
-            if(users.data) { //check if user is already on list (object)
+            if(users[data]) { //check if user is already on list (object)
                 conn.write('\033[93m> nickname already in use. try again:\033[39m ');
                 return;
             }else{
                 nickname = data;
                 users[nickname] = conn;
                 for (var i in users) {
-                    users[i].write('\033[90m> ' + nickname + ' joined the room\033[39m\n');
+                    //users[i].write('\033[90m> ' + nickname + ' joined the room\033[39m\n');
+                    broadcast('\033[90m> ' + nickname + ' joined the room\033[39m\n');
                 }// for
             }// ifelse
         }else{ //consider as a chat message
             for(var i in users) {
                 if(i != nickname) {
-                    users[i].write('\033[39m ' + data + '\n');
+                    //users[i].write('\033[39m ' + data + '\n');
+                    broadcast('\033[39m ' + data + '\n', true);
                 }
             }
         }
@@ -32,6 +41,8 @@ var server = net.createServer(function(conn) { // Create server
     });
     conn.on('close', function(){
         count--;
+        broadcast('\033[90m> ' + nickname + ' left the room\033[39m\n');
+        delete users[nickname]; //delete user form list
     });
 });
 server.listen(9090, function() { // Listen
